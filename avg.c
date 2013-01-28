39d8049a6417615c80c6e0969e34ad543b31adf1
Christoffer Dall <cdall@cs.columbia.edu>
2013-01-28 01:38:45 -0500

    updated avg utility

--- avg.c
+++ avg.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <errno.h>
 
-static double getPower(char *line)
+static int x86mode = 0;
+
+static double get_power(char *line)
 {
 	char *str, *str2, *saveptr = NULL;
 	while (str = strtok_r(line, " \t", &saveptr)) {
@@ -12,32 +17,51 @@ static double getPower(char *line)
 	return atof(str2);
 }
 
-int main(int argc, const char *argv[])
+int main(int argc, char * const argv[])
 {
 	ssize_t ret;
 	size_t bufsize = 0;
 	FILE *stream;
 	char *lineptr = NULL;
+	char *fname;
 	double avg = 0;
 	long long datapoints = 1;
+	int opt;
 
-	if (argc == 2) {
-		stream = fopen(argv[1], "r");
-		if (!stream) {
-			perror("opening file");
-			exit(1);
+	while ((opt = getopt(argc, argv, "x")) != -1) {
+		switch (opt) {
+		case 'x':
+			x86mode = 1;
+			break;
+		default:
+			fprintf(stderr, "Unknown argument: %s\n", argv[optind]);
 		}
-		printf("%s\t", argv[1]);
-	} else {
-		stream = stdin;
 	}
 
+	if (optind >= argc) {
+		stream = stdin;
+	} else {
+		/* first positional argument is file name */
+		fname = argv[optind];
+		stream = fopen(fname, "r");
+		if (!stream) {
+			fprintf(stderr, "Error opening file %s: %s",
+				fname, strerror(errno));
+			exit(1);
+		}
+		printf("%s\t", fname);
+	}
+
+
 	while (getline(&lineptr, &bufsize, stream) > 0) {
-		if (lineptr[0] == '#')
+		if (!x86mode && lineptr[0] == '#')
 			continue;
-		if (!strncmp(lineptr, "time", bufsize))
+		if (!x86mode && !strncmp(lineptr, "time", bufsize))
 			continue;
-		avg = (getPower(lineptr) + (double)datapoints * avg) / ((double)datapoints + 1);
+		if (x86mode && (lineptr[0] < '0' || lineptr[0] > '9'))
+			continue;
+		avg = (get_power(lineptr) + (double)datapoints * avg) /
+			((double)datapoints + 1);
 		datapoints++;
 	}
 
