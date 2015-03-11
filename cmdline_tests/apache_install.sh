459d06fd546f49caddc1aebaa96d8d4fe119f529
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-11 12:55:39 -0600

    don't use https for gcc manual

--- cmdline_tests/apache_install.sh
+++ cmdline_tests/apache_install.sh
@@ -13,6 +13,6 @@ fi
 
 if [[ ! -d "/var/www/html/gcc" ]]; then
 	cd /var/www/html
-	wget https://gcc.gnu.org/onlinedocs/gcc-4.4.7/gcc-html.tar.gz
+	wget http://gcc.gnu.org/onlinedocs/gcc-4.4.7/gcc-html.tar.gz
 	tar xvf gcc-html.tar.gz
 fi
