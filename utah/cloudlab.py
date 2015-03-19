5df22647706020f0489737cac421c26f4aec32d6
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-19 15:51:17 +0100

    udate cloudlab script to include bare-metal

--- utah/cloudlab.py
+++ utah/cloudlab.py
@@ -3,72 +3,58 @@ import geni.rspec.pg as RSpec
 
 
 # Initial setup, parameter passing
-configs = [("both", "1 Xen, 1 KVM, and 1 client node"),
-           ("xen", "1 Xen node and 1 client node"),
-           ("kvm", "1 KVM node and 1 client node")]
-
 pc = portal.Context()
-
-pc.defineParameter("config", "Desired configuration",
-                   portal.ParameterType.NODETYPE, "both", configs)
-
 
 params = pc.bindParameters()
 
 rspec = RSpec.Request()
 
 # kvmarm-3.18-measure disk image URL
-kvm_disk_image = "https://www.utah.cloudlab.us/image_metadata.php?uuid=89e729ae-cd87-11e4-9fb8-3548323d6d11"
+kvm_disk_image = "https://www.utah.cloudlab.us/image_metadata.php?uuid=984d5c44-ce43-11e4-bc0b-020cbce70073"
 # kvmarm-3.18-xen-measure disk image URL
 xen_disk_image = "https://www.utah.cloudlab.us/image_metadata.php?uuid=2221ca56-ce29-11e4-bc0b-020cbce70073"
+# kvmarm-3.18-measure-bm disk image URL
+bm_disk_image = "https://www.utah.cloudlab.us/image_metadata.php?uuid=9f549410-ce44-11e4-9fb8-3548323d6d11"
 
 # Create private LAN
 lan = RSpec.LAN()
 rspec.addResource(lan)
 
-# Parse parameter results
-client = True
-if params.config == "both":
-    xen = True
-    kvm = True
-elif params.config == "xen":
-    xen = True
-    kvm = False
-elif params.config == "kvm":
-    xen = False
-    kvm = True
-else:
-    xen = False
-    kvm = False
-
-
 # Create Client node
-if client:
-    node = RSpec.RawPC("client-node")
-    node.hardware_type = "m400"
-    node.disk_image = kvm_disk_image
-    rspec.addResource(node)
-    iface = node.addInterface("eth1")
-    lan.addInterface(iface)
+# IP will be 10.10.1.1
+node = RSpec.RawPC("client-node")
+node.hardware_type = "m400"
+node.disk_image = kvm_disk_image
+rspec.addResource(node)
+iface = node.addInterface("eth1")
+lan.addInterface(iface)
 
 # Create Xen node
-if xen:
-    node = RSpec.RawPC("xen-node")
-    node.hardware_type = "m400"
-    node.disk_image =xen_disk_image
-    rspec.addResource(node)
-    iface = node.addInterface("eth1")
-    lan.addInterface(iface)
+# IP will be 10.10.1.2
+node = RSpec.RawPC("xen-node")
+node.hardware_type = "m400"
+node.disk_image =xen_disk_image
+rspec.addResource(node)
+iface = node.addInterface("eth1")
+lan.addInterface(iface)
 
 # Create KVM node
-if xen:
-    node = RSpec.RawPC("kvm-node")
-    node.hardware_type = "m400"
-    node.disk_image = kvm_disk_image
-    rspec.addResource(node)
-    iface = node.addInterface("eth1")
-    lan.addInterface(iface)
+# IP will be 10.10.1.3
+node = RSpec.RawPC("kvm-node")
+node.hardware_type = "m400"
+node.disk_image = kvm_disk_image
+rspec.addResource(node)
+iface = node.addInterface("eth1")
+lan.addInterface(iface)
+
+# Create Bare-Metal node
+# IP will be 10.10.1.4
+node = RSpec.RawPC("bm-node")
+node.hardware_type = "m400"
+node.disk_image = kvm_disk_image
+rspec.addResource(node)
+iface = node.addInterface("eth1")
+lan.addInterface(iface)
 
 
 pc.printRequestRSpec(rspec)
-
