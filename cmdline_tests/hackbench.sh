552e3593e18e1edb744eea2907cc0ad05e697d6e
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-20 03:19:48 -0600

    hackbench tweaks

--- cmdline_tests/hackbench.sh
+++ cmdline_tests/hackbench.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-for i in `seq 1 4`; do
-	../tools_arm64/hackbench 100 process 500
+REPTS=${1-4}
+RESULTS=hackbench.txt
+
+for i in `seq 1 $REPTS`; do
+	../tools_arm64/hackbench 100 process 500 | tee >(grep 'Time:' | awk '{ print $2 }' >> $RESULTS)
 done
