f74f0c74d84a0d106ad8c79b2a3bc03cfa2bfba3
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-20 12:55:52 +0100

    set max number of connections

--- cmdline_tests/create_db.sql
+++ cmdline_tests/create_db.sql
@@ -1,3 +1,5 @@
 create user 'sbtest'@'localhost' identified by 'kvm';
 create database sbtest;
 grant all privileges on sbtest.* to 'sbtest'@'localhost' identified by 'kvm';
+set global max_connections=10000;
+set global max_user_connections=0;
