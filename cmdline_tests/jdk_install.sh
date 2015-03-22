f221b21f7e6ff2a88f715d5a8e0b444907adb6ab
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-22 12:54:15 -0600

    install openjdk distro package

--- cmdline_tests/jdk_install.sh
+++ cmdline_tests/jdk_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# First, install distro package and dependencies
+apt-get install -y openjdk-7-jre-headless
+
+
+# Then install the specific release knowen to work with SPEC JVM 2008
 if [[ -e /usr/local/jdk7-server-release-1502 ]]; then
 	echo "/usr/local/jdk7-server-release-1502 already exists, exiting" >&2
 	exit 1
