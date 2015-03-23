7cdab3f5d803e8698d816cd192c977978f22e1e6
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-23 06:18:39 -0600

    put ip in memcache results

--- cmdline_tests/memcached.sh
+++ cmdline_tests/memcached.sh
@@ -4,6 +4,7 @@ SERVER=${1-127.0.0.1}
 REPTS=4
 RESULTS=memcached.txt
 
+echo "Benchmarking $SERVER" | tee >(cat >> $RESULTS)
 for i in `seq 1 $REPTS`; do
 	memtier_benchmark -p 11211 -P memcache_binary -s $SERVER 2>&1 | \
 		tee >(grep '^Totals' -B 4 >> $RESULTS)
