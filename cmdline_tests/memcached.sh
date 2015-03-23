1c73be2c07338c26499485980ff1378abf77368a
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-23 04:25:57 -0600

    update memcached results script

--- cmdline_tests/memcached.sh
+++ cmdline_tests/memcached.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-SERVER=$1
-REPTS=4
+SERVER=${1-127.0.0.1}
+REPTS=8
+RESULTS=memcached.txt
 
 for i in `seq 1 $REPTS`; do
-	memslap --servers $SERVER:11211 --concurrency=100
+	memslap --servers $SERVER:11211 --concurrency=100 | \
+		tee >(grep Took | awk '{ print $2 }' >> $RESULTS)
 done
