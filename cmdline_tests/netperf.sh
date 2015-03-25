046aa480eb8ff3fd2ab8e932d887c9fdf2832cc9
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-25 08:10:06 -0600

    support maerts properly

--- cmdline_tests/netperf.sh
+++ cmdline_tests/netperf.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 SRV=$1
-REPTS=${2-15}
+TEST=${2-ALL}
+REPTS=${3-10}
 RESULTS=netperf.txt
 
 echo "Measuring netperf performance of $SRV"
 
-echo TCP_MAERTS >> $RESULTS
-for i in `seq 1 $REPTS`; do
-	netperf netperf -H $SRV -t TCP_MAERTS | tee >(cat > /tmp/netperf_single.txt)
-	cat /tmp/netperf_single.txt | tail -n 1 | awk '{ print $5 }' >> $RESULTS
-done
-
-echo TCP_STREAM >> $RESULTS
-for i in `seq 1 $REPTS`; do
-	netperf netperf -H $SRV -t TCP_STREAM | tee >(cat > /tmp/netperf_single.txt)
-	cat /tmp/netperf_single.txt | tail -n 1 | awk '{ print $5 }' >> $RESULTS
-done
-
-echo TCP_RR >> $RESULTS
-for i in `seq 1 $REPTS`; do
-	netperf netperf -H $SRV -t TCP_RR | tee >(cat > /tmp/netperf_single.txt)
-	cat /tmp/netperf_single.txt | tail -n 2 | head -n 1 | awk '{ print $6 }' >> $RESULTS
+for _TEST in TCP_MAERTS TCP_STREAM TCP_RR; do
+	if [[ "$TEST" != "ALL" && "$TEST" != "$_TEST" ]]; then
+		continue
+	fi
+	for i in `seq 1 $REPTS`; do
+		netperf netperf -H $SRV -t $_TEST | tee >(cat > /tmp/netperf_single.txt)
+		if [[ $? == 0 ]]; then
+			cat /tmp/netperf_single.txt | tail -n 1 | awk '{ print $5 }' >> $RESULTS
+		fi
+	done
 done
