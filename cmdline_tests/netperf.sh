afcbe3fbf84886a0beadc517bc074cd2d4cdba72
Christoffer Dall <christoffer.dall@linaro.org>
2015-03-25 04:14:37 -0600

    fix netperf script

--- cmdline_tests/netperf.sh
+++ cmdline_tests/netperf.sh
@@ -2,17 +2,18 @@
 
 SRV=$1
 REPTS=${2-15}
+RESULTS=netperf.txt
 
 echo "Measuring netperf performance of $SRV"
 
 echo TCP_STREAM >> $RESULTS
 for i in `seq 1 $REPTS`; do
-	netperf netperf -H 10.10.1.120 -t TCP_STREAM | tee >(cat > /tmp/netperf_single.txt)
+	netperf netperf -H $SRV -t TCP_STREAM | tee >(cat > /tmp/netperf_single.txt)
 	cat /tmp/netperf_single.txt | tail -n 1 | awk '{ print $5 }' >> $RESULTS
 done
 
 echo TCP_RR >> $RESULTS
 for i in `seq 1 $REPTS`; do
-	netperf netperf -H 10.10.1.120 -t TCP_RR | tee >(cat > /tmp/netperf_single.txt)
+	netperf netperf -H $SRV -t TCP_RR | tee >(cat > /tmp/netperf_single.txt)
 	cat /tmp/netperf_single.txt | tail -n 2 | head -n 1 | awk '{ print $6 }' >> $RESULTS
 done
