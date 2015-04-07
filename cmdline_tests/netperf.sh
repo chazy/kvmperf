60e58183306b373147dfd0fea30b7341b9100542
Christoffer Dall <christoffer.dall@linaro.org>
2015-04-07 12:41:54 -0600

    Update netperf client script

--- cmdline_tests/netperf.sh
+++ cmdline_tests/netperf.sh
@@ -11,10 +11,15 @@ for _TEST in TCP_MAERTS TCP_STREAM TCP_RR; do
 	if [[ "$TEST" != "ALL" && "$TEST" != "$_TEST" ]]; then
 		continue
 	fi
+	echo $_TEST >> $RESULTS
 	for i in `seq 1 $REPTS`; do
-		netperf netperf -H $SRV -t $_TEST | tee >(cat > /tmp/netperf_single.txt)
+		netperf -H $SRV -t $_TEST | tee >(cat > /tmp/netperf_single.txt)
 		if [[ $? == 0 ]]; then
-			cat /tmp/netperf_single.txt | tail -n 1 | awk '{ print $5 }' >> $RESULTS
+			if [[ "$_TEST" == "TCP_RR" ]]; then
+				cat /tmp/netperf_single.txt | tail -n 2 | head -n 1 | awk '{ print $6 }' >> $RESULTS
+			else
+				cat /tmp/netperf_single.txt | tail -n 1 | awk '{ print $5 }' >> $RESULTS
+			fi
 		fi
 	done
 done
