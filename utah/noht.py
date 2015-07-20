bb03a28579df3b979671b24491c9fbb4d21fe614
Christoffer Dall <christoffer.dall@linaro.org>
2015-07-20 08:55:55 -0600

    make noht be robust against running again

--- utah/noht.py
+++ utah/noht.py
@@ -6,6 +6,9 @@ import re
 
 def get_core_id(cpu):
     path = '/sys/devices/system/node/node0/%s/topology/core_id' % (cpu)
+    if not os.path.exists(path):
+        return -1
+
     with open(path, 'r') as f:
         coreid = f.readlines()[0].strip()
     return int(coreid)
@@ -16,6 +19,9 @@ p = re.compile('cpu\d+')
 for d in os.listdir('/sys/devices/system/node/node0'):
     if p.match(d):
         core_id = get_core_id(d)
+        if core_id == -1:
+            continue
+
         if core_id in corelist:
             path = '/sys/devices/system/node/node0/%s/online' % (d)
             with open(path, 'w') as f:
