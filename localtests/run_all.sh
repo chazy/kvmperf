fe680d36f40da503667076a100c3873133f94b01
Christoffer Dall <christoffer.dall@linaro.org>
2015-08-05 21:45:10 +0200

    allow run_all.sh to set repetitions for each test

--- localtests/run_all.sh
+++ localtests/run_all.sh
@@ -5,10 +5,27 @@ TIMELOG=$(pwd)/localtest.txt
 rm -f $TIMELOG
 touch $TIMELOG
 
-./pbzip.sh
-./fio.sh
-./kernbench.sh
+usage() {
+	echo "./run_all.sh [pbzip_rpt] [kernbench_rpt] [fio_rpt]" >&2
+	echo "" >&2
+	echo "Default valus:" >&2
+	echo "  pbzip:     3 repts" >&2
+	echo "  kernbench: 1 repts" >&2
+	echo "  fio:       3 repts" >&2
+	exit 0
+}
 
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+	usage
+fi
+
+TEST_PBZIP_REPEAT=${1:-3}
+TEST_KERNBENCH_REPEAT=${2:-1}
+TEST_FIO_REPEAT=${3:-3}
+
+./pbzip.sh $TEST_PBZIP_REPEAT
+./fio.sh $TEST_FIO_REPEAT
+./kernbench.sh $TEST_KERNBENCH_REPEAT
 
 
 cat $TIMELOG
