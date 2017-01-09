77e4ae28016976263c662069878cfa0562b9fa9e
Christoffer Dall <christoffer.dall@linaro.org>
2017-01-09 20:54:07 +0100

    don't start apache server on client

--- cmdline_tests/apache.sh
+++ cmdline_tests/apache.sh
@@ -11,7 +11,7 @@ RESULTS=apache.txt
 ab=/usr/bin/ab
 CMD="$ab -n $NR_REQUESTS -c 100 http://$SRV/gcc/index.html"
 
-service apache2 start
+#service apache2 start
 
 for i in `seq 1 $REPTS`; do
 	$CMD | tee >(grep 'Requests per second' | awk '{ print $4 }' >> $RESULTS)
